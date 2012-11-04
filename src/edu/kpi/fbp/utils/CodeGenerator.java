ae56ffe58d6759fe67a5ca0747f5e370e6978fe8
Michael Pustovit <pustovitm@gmail.com>
2012-11-04 22:41:55 +0200

    Code generation from network object model

---
+++ src/edu/kpi/fbp/utils/CodeGenerator.java
@@ -0,0 +1,107 @@
+package edu.kpi.fbp.utils;
+
+import edu.kpi.fbp.model.ComponentModel;
+import edu.kpi.fbp.model.LinkModel;
+import edu.kpi.fbp.model.NetworkModel;
+
+/**
+ * Code generator which generates Java code from network object model.
+ * @author Pustovit Michael, pustovitm@gmail.com
+ */
+public final class CodeGenerator {
+  /** Main template. */
+  private static final String MAIN_TEMPLATE =
+        "%1$s\n"
+      + "/**\n"
+      + " * Generated network.\n"
+      + " */\n"
+      + "public class %2$s extends ParameterizedNetwork {\n"
+      + "  /**\n"
+      + "   * @param paramStore the parameters store\n"
+      + "   */\n"
+      + "  public %2$s(final ParametersStore paramStore) {\n"
+      + "    super(paramStore);\n"
+      + "  }\n"
+      + "\n"
+      + "  /**\n"
+      + "   * Network definition.\n"
+      + "   * @exception Exception any exceptions\n"
+      + "   */\n"
+      + "  @Override\n"
+      + "  protected void define() throws Exception {\n"
+      + "    // Components\n"
+      + "%3$s\n"
+      + "    // Links\n"
+      + "%4$s"
+      + "  }\n"
+      + "}";
+
+  /** Components and link definition indent. */
+  private static final String INDENT = "    ";
+
+  /** Component declaration template. */
+  private static final String COMPONENT_TEMPLATE = "component(\"%1$s\", %2$s);";
+
+  /** Link declaration template. */
+  private static final String LINK_TEMPLATE = "connect(\"%1$s.%2$s\", \"%3$s.%4$s\");";
+
+  /** Import declaration template. */
+  private static final String IMPORT_TEMPLATE = "import %s;";
+
+  private CodeGenerator() {
+    // do nothing
+  }
+
+  private static String getComponentDefinitions(final NetworkModel model) {
+    final StringBuilder builder = new StringBuilder();
+
+    for (final ComponentModel comp : model.getComponents()) {
+      builder.append(INDENT);
+      builder.append(String.format(COMPONENT_TEMPLATE, comp.getName(), comp.getClassName()));
+      builder.append("\n");
+    }
+
+    return builder.toString();
+  }
+
+  private static String getLinkDefinitions(final NetworkModel model) {
+    final StringBuilder builder = new StringBuilder();
+
+    for (final LinkModel link : model.getLinks()) {
+      builder.append(INDENT);
+      builder.append(
+          String.format(
+              LINK_TEMPLATE,
+              link.getFromComponent(), link.getFromPort(),
+              link.getToComponent(), link.getToPort()));
+      builder.append("\n");
+    }
+
+    return builder.toString();
+  }
+
+  private static String getImports(final NetworkModel model) {
+    final StringBuilder builder = new StringBuilder();
+
+    for (final ComponentModel comp : model.getComponents()) {
+      builder.append(String.format(IMPORT_TEMPLATE, comp.getClassName()));
+      builder.append("\n");
+    }
+
+    return builder.toString();
+  }
+
+  /**
+   * Generates Java code from network object model.
+   * @param networkModel the network object model
+   * @return the Java code string
+   */
+  public static String generate(final NetworkModel networkModel) {
+    final String imports = getImports(networkModel);
+    final String networkName = networkModel.getNetworkName();
+    final String components = getComponentDefinitions(networkModel);
+    final String links = getLinkDefinitions(networkModel);
+
+    return String.format(MAIN_TEMPLATE, imports, networkName, components, links);
+  }
+}
