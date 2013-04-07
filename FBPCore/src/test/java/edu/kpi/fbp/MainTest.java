61305f09506752d3515e3c93560830b24e9d463a
Michael Pustovit <mpustovit@stanfy.com.ua>
2013-04-07 19:04:28 +0300

    Checkstyle and gitignore editions

---
+++ FBPCore/src/test/java/edu/kpi/fbp/MainTest.java
@@ -0,0 +1,22 @@
+package edu.kpi.fbp;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Main test.
+ *
+ * @author Pustovit Michael, pustovitm@gmail.com
+ */
+public class MainTest {
+
+  /**
+   * Test.
+   */
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
